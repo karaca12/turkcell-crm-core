65652d469bbe11543e60b8c7629b9bb398f0af33
karaca <mekaraca18@gmail.com>
2024-04-22 16:56:46 +0300

    1.2 impl

--- core/src/main/java/com/turkcell/pair1/configuration/BaseApplicationConfiguration.java
+++ core/src/main/java/com/turkcell/pair1/configuration/BaseApplicationConfiguration.java
@@ -3,7 +3,6 @@ package com.turkcell.pair1.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.LocaleResolver;
