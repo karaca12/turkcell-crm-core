234ed29cb4e82da50ad746d8bc63664f8eb15194
karaca <mekaraca18@gmail.com>
2024-04-24 19:00:04 +0300

    1.2.6 implemented

--- core/src/main/java/com/turkcell/pair1/configuration/BaseSecurityConfiguration.java
+++ core/src/main/java/com/turkcell/pair1/configuration/BaseSecurityConfiguration.java
@@ -5,26 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
 public class BaseSecurityConfiguration {
-    private final PasswordEncoder passwordEncoder;
-    private final UserDetailsService userDetailsService;
-
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setPasswordEncoder(passwordEncoder);
-        authenticationProvider.setUserDetailsService(userDetailsService);
-        return authenticationProvider;
-    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
